d26a65b4c0ce797fbac4e9d708b23af620071c68
tarunfixigo1 <tarunfixigo@gmail.com>
2025-12-12 11:13:47 +0530

    Changes

--- Model/LoginModel.cs
+++ Model/LoginModel.cs
@@ -5,5 +5,8 @@ namespace JwtAuthAPI.Models
         public string Username { get; set; }
         public string Password { get; set; }
         //asdasd
+
+
+        //asdadasd
     }
 }
--- obj/Debug/net9.0/JWTAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/JWTAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JWTAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f4615d8ec9995ff4a4660e493040b3f6b6c37aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("JWTAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JWTAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
